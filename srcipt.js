b16508e30f5f6ef70415c8c416a0693d5e85bdb1
Sai Siddarth <siddarthsiddu086@gmail.com>
2024-06-09 20:44:30 +0530

    Initial code added

---
+++ srcipt.js
@@ -0,0 +1,63 @@
+function toggleMode() {
+    // Toggle the body class between light-mode and dark-mode
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+    
+    // Change the icon based on the mode
+    const icon = document.getElementById('toggle-icon');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const closeBtn = document.querySelector('.close-btn');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.add('open');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        navLinks.classList.remove('open');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const switchModeButton = document.querySelector('.switch-mode');
+    const body = document.body;
+    const icon = switchModeButton.querySelector('i');
+    const logoImg = document.querySelector('.logo-nav img');
+    const footerImg = document.querySelector('.contact-left-footer img');
+
+
+    switchModeButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        
+        const allTextElements = document.querySelectorAll('body *');
+        allTextElements.forEach(element => {
+            element.classList.toggle('dark-text');
+        });
+        
+        if (body.classList.contains('dark-mode')) {
+            logoImg.src = 'dir/images/logo-white.png';
+        } else {
+            logoImg.src = 'dir/images/logo.png';
+        }
+
+        if (body.classList.contains('dark-mode')) {
+            footerImg.src = 'dir/images/logo-white.png';
+        } else {
+            footerImg.src = 'dir/images/logo.png';
+        }
+
+        icon.classList.toggle('fa-moon');
+        icon.classList.toggle('fa-sun');
+    });
+});
